5b18cfdb88e8baae984e4205d4490f25b83d2626
Parakh Srivastava <srivastavaparakh2017@gmail.com>
2020-01-12 01:08:33 +0530

    Create 10_Second_minimum_BST.py

---
+++ 10_Second_minimum_BST.py
@@ -0,0 +1,44 @@
+'''
+Given a non-empty special binary tree consisting of nodes with the non-negative value, where each node in this tree has exactly two or zero sub-node. If the node has two sub-nodes, then this node's value is the smaller value among its two sub-nodes. More formally, the property root.val = min(root.left.val, root.right.val) always holds.
+
+Given such a binary tree, you need to output the second minimum value in the set made of all the nodes' value in the whole tree.
+
+If no such second minimum value exists, output -1 instead.
+
+Example 1:
+
+Input: 
+    2
+   / \
+  2   5
+     / \
+    5   7
+
+Output: 5
+Explanation: The smallest value is 2, the second smallest value is 5.
+
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def findSecondMinimumValue(self, root: TreeNode) -> int:
+        values = []
+        def getValues(root):
+            if not root:
+                return
+            values.append(root.val)
+            if root.left:
+                getValues(root.left)
+            if root.right:
+                getValues(root.right)
+        getValues(root)
+        values = sorted(set(values))
+        if len(values) > 1:
+            return values[1]
+        return -1        
