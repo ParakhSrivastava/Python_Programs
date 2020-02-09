3ed651507c97f20e3548b9d65b07ad4e1e526262
Parakh Srivastava <srivastavaparakh2017@gmail.com>
2020-02-09 13:33:20 +0530

    Create 67_PowerThree_MATH.py

---
+++ 67_PowerThree_MATH.py
@@ -0,0 +1,21 @@
+'''
+Given an integer, write a function to determine if it is a power of three.
+
+Example 1:
+
+Input: 27
+Output: true
+Example 2:
+
+Input: 0
+Output: false
+'''
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n==0:
+            return False
+        while n%3==0:
+            n=n//3
+        if n==1:
+            return True
