ef31d4b426f8973e141fff3afd3ed746f31b2d65
Parakh Srivastava <srivastavaparakh2017@gmail.com>
2020-02-13 21:55:20 +0530

    Create 82_MaxDepth_TREE.py

---
+++ 82_MaxDepth_TREE.py
@@ -0,0 +1,30 @@
+'''
+Given a n-ary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
+
+Example 1:
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: 3
+'''
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        def dfs(root):
+            if not root:
+                return 0
+            res = 0
+            for child in root.children:
+                res = max(dfs(child), res)
+            return res+1
+        return dfs(root)
