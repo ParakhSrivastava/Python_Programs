9b028888145047eb0ea14dbbd95f0420d72daaae
Parakh Srivastava <srivastavaparakh2017@gmail.com>
2020-02-23 13:57:59 +0530

    Create 106_LongestUnivaluedPath_TREE.py

---
+++ 106_LongestUnivaluedPath_TREE.py
@@ -0,0 +1,54 @@
+'''
+Given a binary tree, find the length of the longest path where each node in the path has the same value. This path may or may not pass through the root.
+
+The length of path between two nodes is represented by the number of edges between them.
+
+ 
+
+Example 1:
+
+Input:
+
+              5
+             / \
+            4   5
+           / \   \
+          1   1   5
+Output: 2
+'''
+
+
+class Solution:
+    
+    def longestUnivaluePath(self, root: TreeNode) -> int:
+        
+        
+        # keep track of longest univalue path
+        longest_uni_path = 0
+        
+        def helper( node):
+            if not node:
+                return 0
+
+            else:
+
+                path_of_left = helper( node.left )
+                path_of_right = helper( node.right )
+                
+                # if left node has the same value, extend uni path
+                left_uni = path_of_left+1 if node.left and node.left.val == node.val else 0
+                
+                # if right node has the same value, extend uni path
+                right_uni = path_of_right+1 if node.right and node.right.val == node.val else 0
+        
+                nonlocal longest_uni_path
+                # use node as bridge to make uni_path as long as possible
+                longest_uni_path = max( longest_uni_path, left_uni + right_uni )
+
+                return max(left_uni, right_uni)        
+        
+		#------------------------------------------------
+        
+        helper( root )
+        
+        return longest_uni_path
