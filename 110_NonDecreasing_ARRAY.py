1df5c17399aafc6cb15dc920d099d4abb1e72005
Parakh Srivastava <srivastavaparakh2017@gmail.com>
2020-03-03 16:48:40 +0530

    Create 110_NonDecreasing_ARRAY.py

---
+++ 110_NonDecreasing_ARRAY.py
@@ -0,0 +1,27 @@
+'''
+Given an array with n integers, your task is to check if it could become non-decreasing by modifying at most 1 element.
+
+We define an array is non-decreasing if array[i] <= array[i + 1] holds for every i (1 <= i < n).
+
+Example 1:
+Input: [4,2,3]
+Output: True
+Explanation: You could modify the first 4 to 1 to get a non-decreasing array.
+Example 2:
+Input: [4,2,1]
+Output: False
+Explanation: You can't get a non-decreasing array by modify at most one element.
+'''
+
+
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        trig=False
+        for i in range(len(nums)-1):
+            if nums[i]>nums[i+1] and trig:
+                return False
+            elif nums[i]>nums[i+1]:
+                if nums[i-1]>nums[i+1] and i!=0:
+                    nums[i+1]=nums[i]
+                trig=True
+        return True
